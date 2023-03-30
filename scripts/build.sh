fe5650d796709ed8968ce717e3f32d81a8e39c17
Richard Gibert <richard.gibert@sentry.io>
2023-03-30 15:13:12 -0400

    force build to amd64

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ npm run build
 mkdir -p $SCRIPT_DIR/../backend/src/public
 cp -r $SCRIPT_DIR/../frontend/build/* $SCRIPT_DIR/../backend/src/public/
 cd $SCRIPT_DIR/../backend; 
-docker buildx build \
-  --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+docker build \
+  --platform linux/amd64 \
   --tag getsentry/github-deployment-gate:latest \
   .
