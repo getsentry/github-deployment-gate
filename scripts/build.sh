47e1b23008d6bb65e635a9b3bb91a331c93a5891
Mike Ihbe <mike.ihbe@sentry.io>
2023-03-30 12:09:49 -0700

    mkdir -p

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../frontend; 
 npm run build
-mkdir $SCRIPT_DIR/../backend/src/public
+mkdir -p $SCRIPT_DIR/../backend/src/public
 cp -r $SCRIPT_DIR/../frontend/build/* $SCRIPT_DIR/../backend/src/public/
 cd $SCRIPT_DIR/../backend; 
 docker buildx build \
