12eba2a465a68150b7259286eb0685f622b37f38
Mike Ihbe <mike.ihbe@sentry.io>
2023-04-05 12:47:11 -0700

    Add some non-secret env configs to FE build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+export REACT_APP_GITHUB_CLIENT_ID="Iv1.5008fddaf6c532b9"
+export REACT_APP_SENTRY_DSN="https://47ec3abc9f494cf4a0ebe83c2a340c4c@o1.ingest.sentry.io/4504922570424320"
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
 cd $SCRIPT_DIR/../frontend && npm run build || true
